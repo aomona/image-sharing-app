e2c4d1f2b0c1ed9713df543d3f2f70b00ec338d8
aomona <yuka41623@gmail.com>
2025-12-04 07:41:54 +0900

    generate migrations

---
+++ migrations/0002_brave_rachel_grey.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "user" ADD COLUMN "header_image" text;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "bio" text;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "location" text;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "website" text;
