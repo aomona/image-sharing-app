841dd80e3b81754d140e9d17b26b9cbfaec0be1d
aomona <yuka41623@gmail.com>
2025-12-05 08:02:19 +0900

    add lint rules "react/no-children-prop off"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,11 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "react/no-children-prop": "off",
+    },
+  },
   eslintConfigPrettier,
 ]);
 
