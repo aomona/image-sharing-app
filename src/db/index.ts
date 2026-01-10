f1db0f7d17e33ad826149bef268a0f4989dbcc1a
aomona <yuka41623@gmail.com>
2026-01-11 03:45:18 +0900

    fix db connection file

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,6 @@
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import ws from "ws";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is not set");
-}
-
-export const db = drizzle({
-  connection: process.env.DATABASE_URL,
-  ws: ws,
-} as const);
+neonConfig.webSocketConstructor = ws;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle({ client: pool });
