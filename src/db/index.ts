0106f45b7947aadcbfce2a132ef228e07dcbf4b0
aomona <yuka41623@gmail.com>
2026-01-10 16:50:34 +0900

    Use neon-serverless and require DATABASE_URL

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,11 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
 
-config({ path: ".env" }); // or .env.local
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+export const db = drizzle({
+  connection: process.env.DATABASE_URL,
+  ws: ws,
+} as const);
